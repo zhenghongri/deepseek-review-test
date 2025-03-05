6548e5c53e354c5e0b4ada3efce15d01a2fe8aad
debug.zheng <405186501@qq.com>
2025-03-05 15:54:50 +0800

    init

---
+++ deepseek-review-test/src/test/java/com/example/deepseekreviewtest/DeepseekReviewTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deepseekreviewtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepseekReviewTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

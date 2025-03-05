deb545c223a7a5f8e28785822588bf5b8a9bb0ac
debug.zheng <405186501@qq.com>
2025-03-05 16:06:21 +0800

    init

---
+++ src/main/java/com/example/deepseekreviewtest/Test.java
@@ -0,0 +1,16 @@
+package com.example.deepseekreviewtest;
+
+public class Test {
+
+    public void _1() {
+        System.out.println("Test 1");
+    }
+
+    public void
+    AAAA() {
+        System
+                .out
+                
+                .println("Test 2");
+    }
+}

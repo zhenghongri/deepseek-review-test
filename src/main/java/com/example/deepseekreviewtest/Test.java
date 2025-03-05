5d5e43ac16febfde9ed8f80ceb2278c2b960fa26
debug.zheng <405186501@qq.com>
2025-03-05 16:12:34 +0800

    init

--- src/main/java/com/example/deepseekreviewtest/Test.java
+++ src/main/java/com/example/deepseekreviewtest/Test.java
@@ -3,14 +3,30 @@ package com.example.deepseekreviewtest;
 public class Test {
 
     public void _1() {
-        System.out.println("Test 1");
+//        这是
+//        中文
+//        注释
+        System.
+
+                out.
+
+                println("Test 1");
+    }
+    public void A_1() {
+//        这是
+//        中文
+//        注释
+        System.
+
+                out.
+
+                println("Test A_1");
     }
 
     public void
     AAAA() {
         System
                 .out
-
                 .println("Test 2");
     }
 }
